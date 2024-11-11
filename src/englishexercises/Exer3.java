2c9463727a92514f0e49a5d095d760e151aa8a4e
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-11-11 11:04:10 +0100

    Exercise 3 NOT finished.

---
+++ src/englishexercises/Exer3.java
@@ -0,0 +1,24 @@
+package englishexercises;
+
+import java.util.*;
+
+public class Exer3 {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		
+		int array[] = new int[10];
+		
+		System.out.print("The array length is set to 10.\n"
+				+ "Type 10 to keep it or type another length\n"
+				+ " to modify it: ");
+		array[] = new int[reader.close();];
+		
+		
+		
+		
+		reader.close();
+
+	}
+
+}
