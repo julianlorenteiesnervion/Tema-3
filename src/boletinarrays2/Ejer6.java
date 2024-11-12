19856d4ca955ad8042420b6a129f75f41a69e3e4
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-11-12 17:45:22 +0100

    Ejercicio 6 NO terminado (preguntar dudas el próximo día de clase).

--- src/boletinarrays2/Ejer6.java
+++ src/boletinarrays2/Ejer6.java
@@ -7,11 +7,17 @@ public class Ejer6 {
 	public static void main(String[] args) {
 		Random random = new Random();
 		
+		int randomTemporal;
+		
 		int[] apuesta = new int[6];
 		int[] ganador = new int[6];
 		
 		for (int i = 0; i < apuesta.length; i++) {
-			apuesta[i] = random.nextInt(1, 50);
+			do {
+			randomTemporal = random.nextInt(1, 50);
+			} while (Arrays.binarySearch(apuesta, randomTemporal) >= 0);
+			
+			apuesta[i] = randomTemporal;
 			Arrays.sort(apuesta);
 			}
 		
