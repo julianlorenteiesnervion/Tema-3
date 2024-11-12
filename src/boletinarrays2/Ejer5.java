59b6b58f715550f6a4f581e9f3e455426d23e3a8
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-11-12 13:33:01 +0100

    Ejercicio 5 terminado.

--- src/boletinarrays2/Ejer5.java
+++ src/boletinarrays2/Ejer5.java
@@ -8,26 +8,37 @@ public class Ejer5 {
 		Random random = new Random();
 		Scanner reader = new Scanner(System.in);
 		
+		// Variable para guardar el número que introduce el usuario
 		int num;
 		
+		/* Contador para guardar las veces que se ha encontrado
+		ese número en el array */
 		int count = 0;
 		
+		// Creación del array con 1000 posiciones
 		int array[] = new int[1000];
 	
+		// For para generar los números aleatorios en el array
 		for (int i = 0; i < array.length; i++) {
 			array[i] = random.nextInt(0, 100);
 		}
 		
+		// Ordenamos el array
 		Arrays.sort(array);
 		
+		// Le pedimos el número al usuario
 		System.out.print("Introduce un número: ");
 		num = reader.nextInt();
 		
-		for (int i = 0; i <= num; i++) {
+		/* For para comprobar si el número que toca es igual
+		al número introducido por el usuario. Si lo es, incrementamos
+		la variable "count" */
+		for (int i = 0; i < array.length; i++) {
 			if (array[i] == num)
 				count++;
 		}
 		
+		// Imprimimos por pantalla al usuario el contador
 		System.out.print("El número " + num + " se ha encontrado"
 				+ " " + count + " veces en este array.");
 		
