c8f6f763e139bdc15b0bac48df63d804c024da1c
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-11-13 10:12:35 +0100

    Ejercicio 2 terminado (falta comentar).

--- src/boletinarrays3/Ejer2.java
+++ src/boletinarrays3/Ejer2.java
@@ -7,23 +7,49 @@ public class Ejer2 {
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 		
-		int[][] notas = new int[5][4];
+		int[][] notas = new int[4][5];
+		
+		int min;
+		int max;
+		double med;
 		
 		for (int a = 0; a < 4; a++) {
 			for (int n = 0; n < 5; n++) {
 				System.out.print("Introduce la nota " + (n+1) + " del alumno " + (a+1) + ": ");
-				notas[n][a] = reader.nextInt();
+				notas[a][n] = reader.nextInt();
 			}
 		}
 		
 		for (int a = 0; a < 4; a++) {
 			for (int n = 0; n < 5; n++) {
-				System.out.print(notas[n][a] + "\t");
+				System.out.print(notas[a][n] + "\t");
 			}
 			System.out.println();
 		}
 		
+		// 4 vueltas para los 4 alumnos
+		for (int a = 0; a < 4; a++) {
+		min = notas[a][0];
+		max = notas[a][0];
+		med = 0;
 		
+			// 5 notas de cada alumno
+			for (int n = 0; n < 5; n++) {
+				if (notas[a][n] < min)
+					min = notas[a][n];
+				if (notas[a][n] > max)
+					max = notas[a][n];
+				
+				med += notas[a][n];
+			}
+		
+		med /= 5;
+		System.out.println("Alumno " + (a+1) + "\nNota mínima: " + min + "\n"
+				+ "Nota máxima: " + max + "\n"
+						+ "Nota media: " + med);
+		
+		System.out.println();
+		}
 		
 		reader.close();
 
