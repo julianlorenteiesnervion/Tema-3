f728fa2f752f5a4461be6eddfb49f8507009b775
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-11-13 10:53:04 +0100

    Ejercicio 2 terminado.

--- src/boletinarrays3/Ejer2.java
+++ src/boletinarrays3/Ejer2.java
@@ -7,20 +7,28 @@ public class Ejer2 {
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 		
+		// Creamos una tabla "notas" con 4 filas y 5 columnas
 		int[][] notas = new int[4][5];
 		
+		/* Declaro las variables para guardar la nota
+		 * mínima, máxima y media */
 		int min;
 		int max;
 		double med;
 		
+		// Vuelta para cada alumno
 		for (int a = 0; a < 4; a++) {
+			// Vueltas para cada nota de cada alumno
 			for (int n = 0; n < 5; n++) {
 				System.out.print("Introduce la nota " + (n+1) + " del alumno " + (a+1) + ": ");
 				notas[a][n] = reader.nextInt();
 			}
 		}
 		
+		// Vuelta para imprimir cada alumno
 		for (int a = 0; a < 4; a++) {
+			System.out.print("Alumno " + (a+1) + " | ");
+			// Vueltas para imprimir cada nota de cada alumno
 			for (int n = 0; n < 5; n++) {
 				System.out.print(notas[a][n] + "\t");
 			}
@@ -43,7 +51,9 @@ public class Ejer2 {
 				med += notas[a][n];
 			}
 		
+		// Media
 		med /= 5;
+		
 		System.out.println("Alumno " + (a+1) + "\nNota mínima: " + min + "\n"
 				+ "Nota máxima: " + max + "\n"
 						+ "Nota media: " + med);
