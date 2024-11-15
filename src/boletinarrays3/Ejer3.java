27e8a09859a115f228ece39b2cac2f53cd10f615
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-11-15 12:53:56 +0100

    Ejercicio 3 terminado.

--- src/boletinarrays3/Ejer3.java
+++ src/boletinarrays3/Ejer3.java
@@ -7,23 +7,28 @@ public class Ejer3 {
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 		
+		// Declaración de variables
 		int filas;
 		int columnas;
 		
+		// Recogemos el número de filas y de columnas
 		System.out.print("Introduce el número de filas: ");
 		filas = reader.nextInt();
 		
 		System.out.print("Introduce el número de columnas: ");
 		columnas = reader.nextInt();
 		
+		// Creamos un array bidimensional
 		int[][] tabla = new int[filas][columnas];
 		
-		for (int i = 0; i < filas; i++) {
-			for (int j = 0; j < columnas; j++) {
-				tabla[j][i] = 10 * i + j;
+		// For para las filas
+		for (int j = 0; j < filas; j++) {
+			// For para las columnas
+			for (int i = 0; i < columnas; i++) {
+				tabla[j][i] = 10 * j + i; // Operación
 				System.out.print(tabla[j][i] + "\t");
 			}
-		System.out.println();
+		System.out.println(); // Salto de línea
 		}
 		
 		reader.close();
