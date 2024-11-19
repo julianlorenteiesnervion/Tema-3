d0da8a35d33ae2d0f558751f719ae1f23ddfb9aa
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-11-19 14:36:20 +0100

    Ejercicio 4 NO terminado.

---
+++ src/boletinfunciones/Ejer4.java
@@ -0,0 +1,27 @@
+package boletinfunciones;
+
+public class Ejer4 {
+	static int buscar(int t[], int clave) {
+		int found = -1;
+		int pos = 0;
+		
+		
+		
+//		for (int value : t) {
+//			if (value == clave)
+//				found = pos;
+//			
+//			pos++;
+//		}
+		
+		return found;
+	}
+	
+
+	public static void main(String[] args) {
+		int[] table = new int[] {3, 4, 5, 6, 7};
+		
+		System.out.print(buscar(table, 6));
+	}
+
+}
