c206f241bf6d4038234255d1e3f63c01fa12fce0
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-11-20 09:23:49 +0100

    Ejercicio 5 actualizado.

--- src/boletinfunciones/Ejer5.java
+++ src/boletinfunciones/Ejer5.java
@@ -11,39 +11,49 @@ public class Ejer5 {
 		// Inicializamos un contador a 0
 		int count = 0;
 		
-		/* Recorremos la tabla que recibimos como entrada
-		 * y en caso de encontrar el valor que buscamos, incrementamos
-		 * el contador que hemos creado anteriormente */
-		for (int value : t) {
-			if (value == valor)
-				count++;
-		}
-		
 		/* Creamos una tabla para guardar los índices,
 		 * que será tan larga como veces haya sido
 		 * encontrado el valor en la tabla recibida */
-		int[] indices = new int[count];
-		
-		/* Restablecemos el contador a 0, ya que ahora
-		 * lo vamos a usar para controlar las posiciones
-		 * de la tabla de los índices */
-		count = 0;
+		int[] indices = new int[longitud(t, valor)];
 		
 		// For que recorrerá toda la tabla recibida
 		for (int i = 0; i < t.length; i++) {
+			/* Si el valor correspondiente es el mismo que el valor recibido,
+			 * añadimos esa posición a la tabla de índices */
 			if (t[i] == valor) {
 				indices[count] = i;
+				// Incremento del contador de posiciones de la tabla indices
 				count++;
 			}
 		}
 		
+		// Devolvemos la tabla
 		return indices;
+	}
+	
+	static int longitud(int t[], int valor) {
+		// Inicializamos un contador a 0
+		int count = 0;
+		
+		/* Recorremos la tabla que recibimos como entrada
+		 * y en caso de encontrar el valor que buscamos, incrementamos
+		 * el contador que hemos creado anteriormente */
+		for (int value : t) {
+			if (value == valor) {
+				count++;
+			}
+		}
+		
+		// Devolvemos el contador
+		return count;
 	}
 
 	public static void main(String[] args) {
 		
-		int[] table = new int[] {2, 3, 4, 5};
+		// Tabla con números
+		int[] table = new int[] {2, 3, 4, 5, 6};
 		
+		// Imprimimos la tabla de los índices
 		System.out.print(Arrays.toString(buscarTodos(table, 3)));
 		
 	}
