52f18a5f922fdb3befc6285610970b1c54be42e8
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-11-25 11:04:45 +0100

    Ejercicio 2 NO terminado.

---
+++ src/ejerciciosjuegos/Ejer2.java
@@ -0,0 +1,20 @@
+package ejerciciosjuegos;
+
+import java.util.Random;
+
+public class Ejer2 {
+	
+	static int[] randomTable(int[] table) {
+		Random random = new Random();
+		
+		for (int i = 0; i < table.length; i++) {
+		}
+		
+		return table;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
