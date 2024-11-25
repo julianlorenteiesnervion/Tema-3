153fd994aab5f17a8de8cb38c62df06bf5660df7
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-11-25 11:14:20 +0100

    Ejercicio 2 NO terminado.

--- src/ejerciciosjuegos/Ejer2.java
+++ src/ejerciciosjuegos/Ejer2.java
@@ -7,8 +7,17 @@ public class Ejer2 {
 	static int[] randomTable(int[] table) {
 		Random random = new Random();
 		
-		for (int i = 0; i < table.length; i++) {
+		int[] pos = new int[table.length];
+		
+		// Generador de posiciones aleatorias
+		for (int i = 0; i < pos.length; i++) {
+			pos[i] = random.nextInt(0, table.length);
+			
+			for (int j = 0; j < pos.length; j++) {
+				if (pos[i] ==)
+			}
 		}
+		
 		
 		return table;
 	}
